1e9edacdcdfe040c02fc7a829c75008cca41e8fe
Spencer <spencerbarclay26@gmail.com>
2026-02-25 15:11:13 -0500

    Player Movement and Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Threading.Tasks;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rb;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.0f;
+    public LayerMask groundMask;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    public bool isGrounded = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void OnJump()
-    {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGround();
+        transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * moveSpeed);
+    }
+
+    void CheckGround()
+    {
+        if (groundCheck == null)
+        {
+            isGrounded = false;
+            return;
+        }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        Debug.Log("Jump");
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
