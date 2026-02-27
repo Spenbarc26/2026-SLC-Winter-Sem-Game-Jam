010e0493cd5e9210c0696dffc578b0c0537591dd
Spencer <spencerbarclay26@gmail.com>
2026-02-27 09:51:55 -0500

    Reapply "Final Changes"
    This reverts commit 20b0a1cdb27658bbcdb43fc6c3efcb1e7dfb4a27.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip birdHit;
     public GameObject life1;
     public GameObject life2;
     public GameObject life3;
@@ -64,6 +66,7 @@ public class PlayerHealth : MonoBehaviour
             Debug.Log("PlayerDamaged");
             lives--;
             UIManager.instance.InstantiateHitUI();
+            source.PlayOneShot(birdHit);
         }
     }
 }
